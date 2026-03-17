a3753b63bf0bce6cc289546a2ee15a715ceea7f6
arisawa <arisawa.teresa@gmail.com>
2026-03-17 15:42:42 -0700

    added drunkenness and sobering up with the beer

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -1,25 +1,99 @@
+using System;
 using UnityEngine;
+using System.Collections;
+
 
 public class Corgi : MonoBehaviour
 {
+    public Sprite DrunkSprite;
+    public Sprite SoberSprite;
+    
     private SpriteRenderer spriteRenderer;
+    private bool isDrunk = false;
+    private Coroutine soberUpCoroutine;
 
     public void Awake()
     {
-        // look at the object it is attached to and wire it up automatically
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();        // looks at the object it is attached to and wire it up automatically
     }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // print("trigger");
+        
+        if (other.tag == "Beer")
+        {
+            print("beer");
+            GetDrunk();
+            Destroy(other);
+        }
+        else if (other.tag == "Bone")
+        {
+            print("bone");
+        }
+        else if (other.tag == "Pill") 
+        {
+            print("pill");
+        }
+    }
+
+    private void GetDrunk()
+    {
+        isDrunk = true;
+        ChangeToDrunkSprite();
+        StartSoberingUp();
+    }
+    
+    private void ChangeToDrunkSprite()
+    {
+        spriteRenderer.sprite = DrunkSprite;
+    }
+
+    private void StartSoberingUp()
+    {
+        if (soberUpCoroutine != null)
+            StopCoroutine(soberUpCoroutine);
+        
+        soberUpCoroutine = StartCoroutine(CountdownUntilSober());
+    }
+
+    IEnumerator CountdownUntilSober()
+    {
+        yield return new WaitForSeconds(GameParameters.CorgiDrunkSeconds);
+        SoberUp();
+    }
+
+    private void SoberUp()
+    {
+        ChangeToSoberSprite();
+        isDrunk = false;
+    }
+
+    private void ChangeToSoberSprite()
+    {
+        spriteRenderer.sprite = SoberSprite;
+    }
+
     
     public void Move(Vector2 direction)
     {
+        direction = ApplyDrunkenness(direction);
+        
         FaceCurrentDirection(direction);
-        
         Vector2 movementAmount = GameParameters.CorgiMoveSpeed * direction * Time.deltaTime;
-        
         spriteRenderer.transform.Translate(movementAmount.x, movementAmount.y, 0);
-        
         spriteRenderer.transform.position = SpriteTools.ConstrainToScreen(spriteRenderer);
-        
+    }
+
+    private Vector2 ApplyDrunkenness(Vector2 direction)
+    {
+        if (isDrunk)
+        {
+            direction.x = direction.x * -1;
+            direction.y = direction.y * -1;
+        }
+
+        return direction; 
     }
 
     private void FaceCurrentDirection(Vector2 direction)
--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 public static class GameParameters
 {
+    // CORGI
     public static float CorgiMoveSpeed = 7f;
+    public static float CorgiDrunkSeconds = 5f;
 
+    // POOP
     public static float PoopSecondsOnScreen = 4f;
     
+    // BEER
     public static float BeerSecondsOnScreen = 7f;
     public static float BeerMinimumSecondsToWait = 1f;
     public static float BeerMaximumSecondsToWait = 3f;
     
+    // BONE
     public static float BoneSecondsOnScreen = 3f;
     public static float BoneMinimumSecondsToWait = 3f;
     public static float BoneMaximumSecondsToWait = 5f;
     
+    // PILL
     public static float PillSecondsOnScreen = 2f;
     public static float PillMinimumSecondsToWait = 4f;
     public static float PillMaximumSecondsToWait = 6f;
